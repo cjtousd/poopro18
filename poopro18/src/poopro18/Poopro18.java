5d914ef2a3b50ae55a9a877e682218fedb6d2334
cjtousd <jimenatousdiaz@gmail.com>
2023-09-19 20:37:17 -0500

    ejercicio   cjtousd

---
+++ poopro18/src/poopro18/Poopro18.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poopro18;
+
+public class Poopro18 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+
+}
